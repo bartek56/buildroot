690820111dda5db903c39581187a847ee5080432
bartek56 <bartosz.brzozowski23@gmail.com>
2020-11-29 19:50:44 +0100

    the newest MediaServer app version

--- package/MediaServer/MediaServer.mk
+++ package/MediaServer/MediaServer.mk
@@ -3,15 +3,9 @@
 # MediaServer
 #
 ################################################################################
-#    $(HOST_DIR)/usr/bin/qmake -o Makefile $(@D)/MediaServer.pro
-#    (cd $(@D); $(TARGET_MAKE_ENV) $(QT_QMAKE))
-#    (cd $(@D); $(QT_QMAKE)  MediaServer.pro)
-#MEDIASERVER_VERSION:= 1.0.0
-#MEDIASERVER_SITE:= /home/programmer/Documents/MediaServer
 
-MEDIASERVER_VERSION = v1.0.1
+MEDIASERVER_VERSION = v1.1
 MEDIASERVER_SITE = $(call github,bartek56,MediaServer,$(MEDIASERVER_VERSION))
-#MEDIASERVER_SITE_METHOD:=git
 MEDIASERVER_INSTALL_STAGING = YES
 MEDIASERVER_INSTALL_TARGET:=YES
 MEDIASERVER_DEPENDENCIES = qt5quickcontrols
